3af990889e7668b58b3ef44d3ac80e3b2fc5d863
Jess <devjessb@gmail.com>
2016-06-12 10:39:36 -0400

    Initial framework with basic js

---
+++ js/resumeBuilder.js
@@ -0,0 +1,44 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
+
+var bio = {};
+bio.name = "Jess Buck";
+bio.role = "Front-End Developer";
+bio.contact = {};
+bio.contact.email = "jessica.e.buck@gmail.com";
+bio.contact.phone = "732-654-2825";
+bio.pictureUrl = "images/me.jpg";
+
+var experience = [];
+var job1 = {};
+job1.position = "Software Developer";
+job1.employer = "Hybrid Healthcare Communications";
+job1.dates = "October 2014 - January 2016";
+experience.push(job1);
+
+var education = [];
+
+var school1 = {};
+school1.name = "Monmouth University";
+school1.dates = "September 2011 - January 2014";
+school1.city = "West Long Branch, NJ";
+school1.degree = "Bachelor of Science in Computer Science";
+education.push(school1);
+
+var school2 = {};
+school2.name = "Brookdale Community College";
+school2.dates = "September 2003 - December 2005";
+school2.city = "Lincroft, NJ";
+school2.degree = "Associate of Arts in English";
+education.push(school2);
+
+var projects = [];
+
+// var formattedName = HTMLheaderName.replace("%data%",bio.name);
+// var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
+// $("#header").prepend(formattedRole);
+// $("#header").prepend(formattedName);
+// $("#main").append(experience[0].position + "<br />" + experience[0].employer + "<br />" +experience[0].dates);
+// $("#main").append("<br /><br />" + education[0].degree + "<br />" + education[0].name + "<br />" +education[0].dates);
+// $("#main").append("<br /><br />" + education[1].degree + "<br />" + education[1].name + "<br />" +education[1].dates);
